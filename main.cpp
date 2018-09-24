4e2865e04a428187b1fc0095dfc722070246317c
David Frode Nilsen <davidfrodenilsen@me.com>
2018-09-24 10:44:58 +0200

    message

--- main.cpp
+++ main.cpp
@@ -21,10 +21,15 @@ int main()
 
     // Create a cola object. Constructor will be called with given parameters.
     Soda cola("Colabrus", 34, 4);
+    Soda farrisGronn("farrisGrønn", 30, 6);
+    Soda juleBrus("juleBrus", 38, 5);
     // We have a variable named cola with Soda as its data type.
 
     // Call the VendingMachine addType function with cola variable as parameter.
     vendingMachine.addType(cola);
+    vendingMachine.addType(farrisGronn);
+    vendingMachine.addType(juleBrus);
+
     // Print inventory using public printInventory member function (method).
     vendingMachine.printInventory();
 
